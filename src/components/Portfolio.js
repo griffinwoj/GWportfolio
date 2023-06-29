76bca638b3730ee4f4cab7d368a362d5ed050ccd
griffinw@protonmail.com <griffinw@protonmail.com>
2023-06-29 18:50:17 -0400

    added portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -20,57 +20,57 @@ function Portfolio() {
           {[
             {
               title: "Blog",
-              description: "CMS-Style Model View Controller Blog",
+              description: " Blog",
               imgSrc: Blog,
               deployedLink:
-                "https://thomas-model-view-controller.herokuapp.com/",
+                "",
               githubLink:
-                "https://github.com/ThomasCalle/Thomas-Model-View-Controller-MVC-Tech-Blog",
+                "",
             },
             {
-              title: "Stack Dev Portfolio",
-              description: "Built On Advanced CSS",
+              title: "S Portfolio",
+              description: "CSS",
               imgSrc: StackDev,
               deployedLink:
-                "https://thomascalle.github.io/ThomasCalleStackDevPortfolio/",
+                "",
               githubLink:
-                "https://github.com/ThomasCalle/ThomasCalleStackDevPortfolio",
+                "",
             },
             {
               title: "Online Workday Planner",
               description: "Third-Party API Sceduler ft. Day.js",
               imgSrc: WorkDayPlanner,
               deployedLink:
-                "https://thomascalle.github.io/Online-Workday-Planner/",
+                "",
               githubLink:
-                "https://github.com/ThomasCalle/Online-Workday-Planner",
+                "",
             },
             {
               title: "Java Script: WEB API Quiz",
               description: "Dynamically updated HTML & CSS Web API quiz game.",
               imgSrc: WebAPI,
               deployedLink:
-                "https://thomascalle.github.io/JavaScript_Interview_Assessment/",
+                "",
               githubLink:
-                "https://github.com/ThomasCalle/JavaScript_Interview_Assessment",
+                "",
             },
             {
               title: "10 Things to Do in Toronto",
               description: "Interactive Front-End Application Guide.",
               imgSrc: ThingsToDoInToronto,
               deployedLink:
-                "https://thomascalle.github.io/10-Things-To-Do-In-Toronto/",
+                "",
               githubLink:
-                "https://github.com/ThomasCalle/10-Things-To-Do-In-Toronto#links",
+                "",
             },
             {
               title: "Weather Dashboard",
-              description: "Server-Side APIs: (5) Day Weather Dashboard.",
+              description: "Weather Dashboard.",
               imgSrc: Weather,
               deployedLink:
-                "https://thomascalle.github.io/Thomas-Weather-Dashboard/",
+                "",
               githubLink:
-                "https://github.com/ThomasCalle/Thomas-Weather-Dashboard",
+                "",
             },
           ].map((project, index) => (
             // Div container for each project
