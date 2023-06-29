1e12acd917e66b0bffbd16b92a4a947b4140c984
griffinw@protonmail.com <griffinw@protonmail.com>
2023-06-29 18:59:13 -0400

    added components

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col, Card, Image } from "react-bootstrap";
-import avatar from "";
+import avatar from "images/G.G.S.WebLoGOWorking.pdf";
 import "../style/About.css";
 
 function About() {
