0dbf405ecb244052b85cdc2e0d1c72e2b1df6c34
griffinw@protonmail.com <griffinw@protonmail.com>
2023-07-26 19:48:15 -0400

    added my personal logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,40 +2,34 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Navigation from "./Navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChessKnight } from "@fortawesome/free-solid-svg-icons";
+import { faMusic } from "@fortawesome/free-solid-svg-icons";
 import { Container, Row, Col } from "react-bootstrap";
 import "../style/Header.css";
-
+import gSlogo from "../images/griffinstudio.png"; // Adjust the path as per your project structure
 
 // Header component
 function Header() {
   return (
-    // Header component
     <>
       <Container fluid className="bg-dark">
         <Row className="justify-content-center text-center mb-0">
           <Col className="pb-0">
             <h1 className="text-white" style={{ fontSize: "70px" }}>
-              <FontAwesomeIcon
-                icon={faChessKnight}
-                className="fa-knight"
-                size="xl"
-                style={{ color: "#white" }}
-              />
-              <Link
-                to="https://github.com/griffinwoj/GWportfolio"
-                className="text-white text-decoration-none"
-                style={{ margin: "0" }}
-              >
-                Griffin Wojtowicz
-              </Link>
-              <FontAwesomeIcon
-                icon={faChessKnight}
+              {/* <FontAwesomeIcon
+                icon={faMusic}
                 flip="horizontal"
                 className="fa-knight"
                 size="xl"
                 style={{ color: "#white" }}
-              />
+              /> */}
+              <img src={gSlogo} alt="Griffin Studio Logo" className="gIcon" />
+              {/* <FontAwesomeIcon
+                icon={faMusic}
+                flip="horizontal"
+                className="fa-knight"
+                size="xl"
+                style={{ color: "#white" }}
+              /> */}
             </h1>
           </Col>
         </Row>
@@ -44,5 +38,6 @@ function Header() {
     </>
   );
 }
+
 // Export Header
 export default Header;
